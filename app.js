8673bb92ad654b6a4d6b50afc6510961a47f753e
CarlosHerrarte <26003850@galileo.edu>
2026-06-21 07:53:28 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 // CONFIGURACIÓN: reemplaza con tu config de Firebase (ver instrucciones)
 const firebaseConfig = {
-  apiKey: "REPLACE_API_KEY",
-  authDomain: "REPLACE_AUTH_DOMAIN",
-  projectId: "REPLACE_PROJECT_ID",
-  storageBucket: "REPLACE_STORAGE_BUCKET",
-  messagingSenderId: "REPLACE_MSG_SENDER",
-  appId: "REPLACE_APP_ID"
+  apiKey: "AIzaSyAm-qj9NaW3L-_tlTX8kJ1FKfFg3G1UrIY",
+  authDomain: "hat-temporal-ag.firebaseapp.com",
+  projectId: "chat-temporal-ag",
+  storageBucket: "chat-temporal-ag.firebasestorage.app",
+  messagingSenderId: "995109559252",
+  appId: "1:995109559252:web:ab4ce8f42d9a152e53d441",
+  measurementId: "G-N7SNC1WH5B"
 };
 
 // Clave requerida (según tu requerimiento)
